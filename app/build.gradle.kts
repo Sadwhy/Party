3d94b6d5b12e6bea0634b7615c623ed05483e843
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-04-17 20:03:31 +0600

    more fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "io.sadwhy.party"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "io.sadwhy.party"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
