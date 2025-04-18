27ad07090a6b4d4f14cf4b6ba5fd4ad2ee4a5960
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-04-19 02:49:40 +0600

    first image load

--- app/src/main/java/io/sadwhy/party/ui/LibraryFragment.kt
+++ app/src/main/java/io/sadwhy/party/ui/LibraryFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import coil3.load
 import io.sadwhy.party.databinding.LibraryFragmentBinding
 
 class LibraryFragment : Fragment() {
@@ -23,7 +24,8 @@ class LibraryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // TODO: use binding to access views
+        
+        binding.postImage.load("https://d.fixupx.com/DarkOddCon/status/1912812028210913632")
     }
 
     override fun onDestroyView() {
