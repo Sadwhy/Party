8a06d4282753d6c63fecf5ba4bd01ae0b57b3f54
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-04-19 02:39:11 +0600

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,10 +45,14 @@ dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.13.0-alpha12")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
     // UI
     implementation("androidx.navigation:navigation-fragment-ktx:2.8.9")
     implementation("androidx.navigation:navigation-ui-ktx:2.8.9")
-    implementation("com.google.android.flexbox:flexbox:3.0.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+
+    // Image loaded
+    implementation("io.coil-kt.coil3:coil-compose:3.1.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.1.0")
 }
---
+++ app/src/main/java/io/sadwhy/party/App.kt
@@ -0,0 +1,41 @@
+package io.sadwhy.party
+
+import android.app.Application
+import android.content.Context
+import android.os.Build.VERSION.SDK_INT
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
+import coil3.disk.DiskCache
+import coil3.disk.directory
+import coil3.memory.MemoryCache
+import coil3.request.allowHardware
+import coil3.request.crossfade
+
+class Party : Application(), SingletonImageLoader.Factory {
+    override fun onCreate() {
+        super.onCreate()
+
+    override fun newImageLoader(context: PlatformContext): ImageLoader {
+        return ImageLoader.Builder(context)
+            .memoryCache {
+                MemoryCache.Builder()
+                    .maxSizePercent(context, 0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(cacheDir.resolve("image_cache"))
+                    .maxSizeBytes(1024 * 1024 * 100) // 100MB
+                    .build()
+            }
+            .allowHardware(false)
+            .crossfade(true)
+            .build()
+    }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        // You can perform early initializations here if needed
+    }
+}
