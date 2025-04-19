45899e41ec3ca6daed919e9e96d5f1ce3a723fc5
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-04-19 23:36:42 +0600

    update

--- app/src/main/java/io/sadwhy/party/MainActivity.kt
+++ app/src/main/java/io/sadwhy/party/MainActivity.kt
@@ -2,8 +2,11 @@ package io.sadwhy.party
 
 import android.os.Bundle
 import android.graphics.Color.TRANSPARENT
+import android.view.View
 import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.NavController
+import androidx.core.content.ContextCompat
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -26,5 +29,35 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNav.setupWithNavController(navController)
+
+        val toolbar = binding.topAppBar
+        setSupportActionBar(toolbar)
+        
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment -> {
+                    toolbar.title = "Home"
+                    toolbar.navigationIcon = null // Remove nav icon
+                    toolbar.menu.clear()
+                }
+                R.id.searchFragment -> {
+                    toolbar.title = "Search"
+                    toolbar.setNavigationIcon(R.drawable.ic_back)
+                    toolbar.menu.clear()
+                }
+                R.id.libraryFragment -> {
+                    toolbar.title = "Library"
+                    toolbar.navigationIcon = null
+                    toolbar.menu.clear()
+                }
+                else -> {
+                    toolbar.title = ""
+                    toolbar.navigationIcon = null
+                    toolbar.menu.clear()
+                }
+            }
+        }
+        
+        
     }
 }
