e8690d204ec194124fde394a15a2daef735e39f3
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-04-19 17:52:20 +0600

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,5 @@ dependencies {
     implementation("io.coil-kt.coil3:coil-compose:3.1.0")
     implementation("io.coil-kt.coil3:coil-network-okhttp:3.1.0")
     implementation("com.google.android.flexbox:flexbox:3.0.0")
+    implementation("io.getstream:photoview:1.0.3")
 }
--- app/src/main/java/io/sadwhy/party/ui/LibraryFragment.kt
+++ app/src/main/java/io/sadwhy/party/ui/LibraryFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import coil3.load
+import coil3.clear
+import io.getstream.photoview.dialog.PhotoViewDialog
 import io.sadwhy.party.databinding.LibraryFragmentBinding
 
 class LibraryFragment : Fragment() {
@@ -29,7 +31,24 @@ class LibraryFragment : Fragment() {
             if (isChecked) {
                 binding.postImage.load("https://d.fixupx.com/DarkOddCon/status/1912812028210913632")
                 binding.postImageA.load("https://d.fixupx.com/lazycataw/status/1912738980879016263")
+            } else {
+                binding.postImage.clear()
+                binding.postImageA.clear()
             }
+        }
+        
+        val imageUrls = listOf(
+          "https://d.fixupx.com/DarkOddCon/status/1912812028210913632",
+          "https://d.fixupx.com/lazycataw/status/1912738980879016263",
+          "https://d.fixupx.com/FeintHeart721/status/1913544604726837686",
+          "https://d.fixupx.com/panpianoatelier/status/1913206722485002329"
+        )
+        
+        val button = binding.postImage
+        button.setOnClickListener {
+          PhotoViewDialog.Builder(context = this, images = imageUrls) { imageView, url ->
+            imageView.load(url)
+          }.build().show()
         }
     }
 
