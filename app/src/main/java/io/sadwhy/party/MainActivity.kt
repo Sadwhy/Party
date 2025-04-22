15a07c8cd47441b65f72f401cbde8ed79cd8e921
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-04-22 08:09:41 +0600

    quick fix

--- app/src/main/java/io/sadwhy/party/MainActivity.kt
+++ app/src/main/java/io/sadwhy/party/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.appbar.MaterialToolbar
 import io.sadwhy.party.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -28,12 +27,11 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        // Hook up bottom navigation with nav controller
+        // Setup bottom nav with nav controller
         binding.bottomNav.setupWithNavController(navController)
 
-        // Hook up top app bar with nav controller
-        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        // Setup top app bar with nav controller
         val appBarConfiguration = AppBarConfiguration(navController.graph)
-        topAppBar.setupWithNavController(navController, appBarConfiguration)
+        binding.topAppBar.setupWithNavController(navController, appBarConfiguration)
     }
 }
