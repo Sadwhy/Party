090b8b703beda6bad465290c6b513ed4f38cc1dd
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-04-22 13:27:50 +0600

    update

--- app/src/main/java/io/sadwhy/party/MainActivity.kt
+++ app/src/main/java/io/sadwhy/party/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import io.sadwhy.party.databinding.ActivityMainBinding
 
@@ -25,6 +26,12 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+
+        // Setup bottom nav with nav controller
         binding.bottomNav.setupWithNavController(navController)
+
+        // Setup top app bar with nav controller
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.topAppBar.setupWithNavController(navController, appBarConfiguration)
     }
 }
