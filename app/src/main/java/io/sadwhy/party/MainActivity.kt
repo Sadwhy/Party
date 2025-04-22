1925247735e25c2c0e3d812c2e9d99c3a3670763
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-04-22 13:23:26 +0600

    revert scroller and add behavior to fragment holder

--- app/src/main/java/io/sadwhy/party/MainActivity.kt
+++ app/src/main/java/io/sadwhy/party/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import io.sadwhy.party.databinding.ActivityMainBinding
 
@@ -26,12 +25,6 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-
-        // Setup bottom nav with nav controller
         binding.bottomNav.setupWithNavController(navController)
-
-        // Setup top app bar with nav controller
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
-        binding.topAppBar.setupWithNavController(navController, appBarConfiguration)
     }
 }
