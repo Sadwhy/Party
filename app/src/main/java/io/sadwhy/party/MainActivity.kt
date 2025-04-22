84771d5f02226b59549ab9276a091a17ea05dc31
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-04-22 08:05:36 +0600

    hacky scroll workaround

--- app/src/main/java/io/sadwhy/party/MainActivity.kt
+++ app/src/main/java/io/sadwhy/party/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
 import io.sadwhy.party.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -25,6 +27,13 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+
+        // Hook up bottom navigation with nav controller
         binding.bottomNav.setupWithNavController(navController)
+
+        // Hook up top app bar with nav controller
+        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        topAppBar.setupWithNavController(navController, appBarConfiguration)
     }
 }
