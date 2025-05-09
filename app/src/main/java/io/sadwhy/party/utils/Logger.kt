dba89a8eec3d6db3990c01e0d22538abf090ac81
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-05-09 23:00:59 +0600

    update Logger.kt

--- app/src/main/java/io/sadwhy/party/utils/Logger.kt
+++ app/src/main/java/io/sadwhy/party/utils/Logger.kt
@@ -6,16 +6,15 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-fun logToFile(message: String, fileName: String? = "log.txt") {
-    val context = Party.appContext
-    val logFile = File(context.filesDir, fileName)
-    val timestamp = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(Date())
-
+fun log(message: String, fileName: String? = "log.txt") {
     try {
+        val context = Party.appContext
+        val logFile = File(context.filesDir, fileName)
+        val timestamp = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(Date())
         FileWriter(logFile, true).use { writer ->
             writer.append("[$timestamp] $message\n")
         }
-    } catch (e: IOException) {
+    } catch (e: Exception) {
         e.printStackTrace()
     }
 }
