a49b13ac3349197537ce5a245fb211db86c04621
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-05-26 21:14:23 +0600

    move search to compose

--- app/src/main/java/io/sadwhy/party/ui/main/search/SearchFragment.kt
+++ app/src/main/java/io/sadwhy/party/ui/main/search/SearchFragment.kt
@@ -2,19 +2,28 @@ package io.sadwhy.party.ui.main.search
 
 import android.os.Bundle
 import android.view.View
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
+import androidx.compose.ui.platform.ComposeView
 import io.sadwhy.party.R
 import io.sadwhy.party.databinding.SearchFragmentBinding
+import io.sadwhy.party.ui.theme.AppTheme
 import io.sadwhy.party.utils.AutoClearedValue.Companion.autoCleared
 
 class SearchFragment : Fragment(R.layout.search_fragment) {
     private var binding: SearchFragmentBinding by autoCleared()
-
-    override fun onViewCreated(
-        view: View,
-        savedInstanceState: Bundle?,
-    ) {
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // TODO: binding usable
+        binding = SearchFragmentBinding.bind(view)
+    
+        binding.searchCompose.setViewCompositionStrategy(
+            ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+        )
+        binding.searchCompose.setContent {
+            AppTheme {
+                SearchScreen("Search", "Compose Search Screen")
+            }
+        }
     }
 }
---
+++ app/src/main/java/io/sadwhy/party/ui/main/search/SearchScreen.kt
@@ -0,0 +1,44 @@
+package io.sadwhy.party.ui.main.home
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SearchScreen(title: String, text: String) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = text,
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
