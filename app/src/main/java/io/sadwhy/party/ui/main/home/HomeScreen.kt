371aebed13c1ffce9886ee399cb7a11a88c0ba59
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-05-26 14:20:54 +0600

    update HomeScreen.kt

--- app/src/main/java/io/sadwhy/party/ui/main/home/HomeScreen.kt
+++ app/src/main/java/io/sadwhy/party/ui/main/home/HomeScreen.kt
@@ -1,21 +1,9 @@
 package io.sadwhy.party.ui.main.home
 
-import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        contentAlignment = Alignment.TopStart
-    ) {
-        Text("Home Fragment (Compose)", fontSize = 20.sp)
-    }
+    Text("Hello from Compose")
 }
