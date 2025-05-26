89435b8d04fdd8a8d4e30921264930fcca2fc61f
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-05-26 22:03:27 +0600

    update CreatorScreen.kt

--- app/src/main/java/io/sadwhy/party/ui/creator/CreatorScreen.kt
+++ app/src/main/java/io/sadwhy/party/ui/creator/CreatorScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.nestedScroll
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
@@ -29,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
