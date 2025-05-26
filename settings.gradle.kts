29a3f187172ea8df15bf0b2762a5e5e84098d25b
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-05-26 15:24:46 +0600

    even more dependency mess

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,20 @@ pluginManagement {
         maven("https://jitpack.io")
     }
 }
+
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
     versionCatalogs {
         create("libs") {
             from(files("gradle/libs.versions.toml"))
         }
     }
 }
+
 rootProject.name = "su-party"
 include(":app")
