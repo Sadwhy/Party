e84b4087f74f4c413f6cf67b23c7b8add02bab6f
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-06-14 11:57:27 +0600

    import fix release

--- app/src/main/java/io/sadwhy/party/ui/media/PostItem.kt
+++ app/src/main/java/io/sadwhy/party/ui/media/PostItem.kt
@@ -22,6 +22,7 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
