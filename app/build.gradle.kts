bc83eb495943fa14df191d8f56b1f4077ee4d56d
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-07-06 00:11:20 +0600

    release remove stability config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,6 @@ android {
 
 composeCompiler {
     reportsDestination = layout.buildDirectory.dir("compose_compiler")
-    stabilityConfigurationFile = rootProject.layout.projectDirectory.file("stability_config.conf")
 }
 
 dependencies {
