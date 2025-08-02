eefaae5c1057e7dd636b3d48f7968480ef1d8182
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-08-03 00:06:45 +0600

    add docs to Attachment data class

--- app/src/main/java/io/sadwhy/party/data/model/Attachment.kt
+++ app/src/main/java/io/sadwhy/party/data/model/Attachment.kt
@@ -3,11 +3,27 @@ package io.sadwhy.party.data.model
 import android.webkit.MimeTypeMap
 import kotlinx.serialization.Serializable
 
+/**
+ * Represents a media attachment associated with a post.
+ *
+ * @property name Optional name of the file.
+ * @property path Server-relative path to the file. Does not include domain.
+ *
+ * The media type is determined based on the file extension and is used internally
+ * to decide whether the UI should display it as an image, video, or generic downloadable file.
+ */
 @Serializable
 data class Attachment(
     val name: String? = null,
     val path: String,
 ) {
+    /**
+     * Determines the type of media based on the MIME type derived from the file extension.
+     * 
+     * - Returns [MediaType.IMAGE] if the file is an image.
+     * - Returns [MediaType.VIDEO] if the file is a video.
+     * - Returns [MediaType.FILE] for all other types.
+     */
     val mediaType: MediaType
         get() {
             val extension = MimeTypeMap.getFileExtensionFromUrl(path).lowercase()
@@ -22,6 +38,12 @@ data class Attachment(
         }
 }
 
+/**
+ * Internal media type categorization for attachments.
+ *
+ * This enum is **not** part of the serialized JSON.
+ * It is used internally to determine how an attachment should be rendered or interacted with.
+ */
 @Serializable
 enum class MediaType {
     IMAGE,
