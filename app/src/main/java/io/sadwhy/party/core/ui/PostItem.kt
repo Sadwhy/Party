9f6bec8630363606250605a9f12bb6c709e73836
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-08-07 10:46:38 +0600

    update PostItem.kt release

--- app/src/main/java/io/sadwhy/party/core/ui/PostItem.kt
+++ app/src/main/java/io/sadwhy/party/core/ui/PostItem.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ColorProvider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.IconButton
@@ -37,6 +36,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorProducer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
