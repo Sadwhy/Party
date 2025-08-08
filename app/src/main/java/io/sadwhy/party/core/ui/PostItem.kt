186acb9dd4a99c0fe5bd7d9de424247b94e922e7
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-08-09 02:28:58 +0600

    add missing import

--- app/src/main/java/io/sadwhy/party/core/ui/PostItem.kt
+++ app/src/main/java/io/sadwhy/party/core/ui/PostItem.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.EaseInSine
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
