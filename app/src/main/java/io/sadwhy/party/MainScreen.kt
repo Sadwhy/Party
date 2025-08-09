b45698749ecc4b6702c4cecc797f2245c837f95c
Sadwhy <224530651+Sadwhy@users.noreply.github.com>
2025-08-09 13:17:55 +0600

    match class to file name. removed some wild cards

--- app/src/main/java/io/sadwhy/party/MainScreen.kt
+++ app/src/main/java/io/sadwhy/party/MainScreen.kt
@@ -4,7 +4,12 @@ import androidx.annotation.DrawableRes
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
